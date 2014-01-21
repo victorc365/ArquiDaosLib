4e98099c6d90423f37afa35c4308ce0fd64220cb
victorc365 <vhcontreraso@unal.edu.co>
2014-01-21 00:28:28 -0500

    Add login method Add signature to method login

--- src/com/superenvios/model/Persona.java
+++ src/com/superenvios/model/Persona.java
@@ -23,7 +23,8 @@ import javax.persistence.NamedQuery;
  */
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-@NamedQueries({@NamedQuery(name = "findAll",query = "SELECT p FROM Persona p")
+@NamedQueries({@NamedQuery(name = "findAll",query = "SELECT p FROM Persona p"),
+    @NamedQuery(name = "findAllWithPermisos",query = "SELECT p FROM Persona p JOIN FETCH p.rol")
 })
 public class Persona implements Serializable {
     private static final long serialVersionUID = 1L;
